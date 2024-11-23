8b1f5dd5bbc6438ea7c24812b41640d09fe3af62
KG0517 <taracolo0903@gmail.com>
2024-11-23 14:42:58 +0900

    fix/udpate cors error

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental:{appDir: false}
-}
+  experimental: { appDir: false },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${process.env.NEXT_PUBLIC_API_BASE_URL}:path*`,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
